40fae1201c13868ad3f5c529b374204ebc457dfc
TTHOAA <rsoto051@fiu.edu>
2019-10-28 20:04:29 -0400

    Adding event listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,5 +3,40 @@ const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 const randomizeLetter = () => {
     let k=Math.floor(Math.random()*26);
     let luckyLetter = letters[k];
-    console.log(luckyLetter);
-}
+    return luckyLetter;
+};
+
+let guesses = [];
+let victories=0;
+let defeats=0;
+let attempts = 8;
+
+document.getElementById('wins').append(victories);
+
+document.getElementById('losses').append(defeats);
+
+document.getElementById('remainder').append(attempts);
+
+document.addEventListener('keyup',function(){
+    let x = event.key;
+    let z = x.toUpperCase();
+    console.log(z);
+    if(x == luckyLetter){
+        victories = victories + 1;
+        attempts = 8;
+        guesses = [];
+    }
+
+  
+})
+
+
+
+
+
+    
+    
+
+
+
+
