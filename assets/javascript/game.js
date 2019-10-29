e37857fe6b910955fd2e088875c6c36cca02ead2
TTHOAA <rsoto051@fiu.edu>
2019-10-29 16:39:42 -0400

    Set up eventlistener. Created checks to only accept letters. Now resets luckyNumber after a win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,9 @@ const randomizeLetter = () => {
     return luckyLetter;
 };
 
+let luckyLetter = randomizeLetter();
+console.log('Lucky letter is ' + luckyLetter);
+
 let guesses = [];
 let victories=0;
 let defeats=0;
@@ -20,11 +23,27 @@ document.getElementById('remainder').append(attempts);
 document.addEventListener('keyup',function(){
     let x = event.key;
     let z = x.toUpperCase();
+    const guesscheck = guesses.includes(' ' + z);
+    const letterCheck =letters.includes(z);
     console.log(z);
-    if(x == luckyLetter){
+    
+    if (guesscheck == false && letterCheck == true){
+        guesses.push(' ' + z);
+    }
+    document.getElementById('guesses').innerHTML= guesses;
+    if(z == luckyLetter){
         victories = victories + 1;
+        document.getElementById('wins').innerHTML=victories;
+        
+       
         attempts = 8;
+        document.getElementById('remainder').innerHTML=attempts;
+        
         guesses = [];
+
+        luckyLetter = randomizeLetter();
+        console.log('Lucky letter is ' + luckyLetter);
+        
     }
 
   
